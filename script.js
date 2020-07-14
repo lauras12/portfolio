65b38e5a7e78da8b00df5f2a1416dfa0fd7e6b14
lauras12 <56985938+lauras12@users.noreply.github.com>
2020-07-14 09:25:18 -0400

    Updated with new app
    updated HTML & CSS to include my new API application (Crypto Info APP) for Hack Capstone project

--- script.js
+++ script.js
@@ -1,55 +1,25 @@
 function onLoad() {
-    $('.burger').click(() => $('nav > ul').toggleClass('navHidden'));
-  }
-  
-  $(onLoad)
+  $('.burger').click(() => $('nav > ul').toggleClass('navHidden'));
+}
 
+$(onLoad)
 
-
-  function scrollToTop() { 
-    $(window).scrollTop(0); 
-  }
+function scrollToTop() { 
+  $(window).scrollTop(0); 
+}
 
 
 
 $(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 450
-       ) {
-	    $("nav").css("background" , "white");
-	  }
+$(window).scroll(function(){
+  var scroll = $(window).scrollTop();
+  if (scroll > 450
+     ) {
+    $("nav").css("background" , "white");
+  }
 
-	  else{
-		  $("nav").css("background" , "transparent");  	
-	  }
-  })
+  else{
+    $("nav").css("background" , "transparent");  	
+  }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
